cdf89cd04e630bae9156489c5ca763bb5e1d163b
Bartlomiej95 <panfilbartlomiej@gmail.com>
2021-01-12 19:31:03 +0100

    Build basic components
    Basic components: Buttons, Headings, Paragraphs, Inputs and
    style them.

--- src/theme/mainTheme.js
+++ src/theme/mainTheme.js
@@ -1,3 +1,66 @@
 export const theme ={
-    color: 'black',
+    
+    // Style do podstawowych komponentów
+    buttonsBorderRadius: '20px',
+
+    buttonsPrimaryBcgColor: '#0903B0',
+    buttonsPrimaryHoverBcgColor: '#372FFF',
+    buttonsPrimaryColor: 'white',
+    
+    buttonsSecondaryBcgColor: 'white',
+    buttonsSecondaryHoverBcgColor: '#372FFF',
+    buttonsSecondaryColor: '#372FFF',
+    buttonsSecondaryHoverColor: 'white',
+
+
+    // Buttons - begin
+    LoginButton: {
+        mobileWidth: '154px',
+        mobileHeight: '40px',
+    },
+
+    IdLoginButton: {
+        mobileWidth: '260px',
+        mobileHeight: '40px',
+    },
+    // Buttons - end
+
+    
+    // Headings - begin
+    Heading: {
+        mobileFontSize: '28px',
+    },
+
+    SubHeading: {
+        mobileFontSize: '18px',
+    },
+
+    SubSubHeading: {
+        mobileFontSize: '15px',
+    },
+
+    // Headings - end
+
+    // Inputs - begin
+    Input:{
+        mobileWidth: '100px',
+        mobileHeight: '40px',
+        borderRadius: '20px',
+        mobilePadding: '10px auto 10px 15px'
+    },
+    // Inputs - end
+
+    // Paragraph - begin
+    Paragraph: {
+        mobileFontSize: '12px',
+    },
+
+    LPParagraph: {
+        mobileFontSize: '50px',
+        
+    }
+    // Paragraph - end
+
+
+
 }
