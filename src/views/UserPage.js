eebd788db769bc2afe6a7c3b77363ff29ca526e3
Bartlomiej95 <panfilbartlomiej@gmail.com>
2021-02-15 20:06:45 +0100

    Delete unuseless useEffect from UserPage

--- src/views/UserPage.js
+++ src/views/UserPage.js
@@ -1,25 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import Footer from '../organisms/Footer';
 import Header from '../organisms/Header';
 import MainSection from '../organisms/MainSection.tsx';
 import ProfileSection from '../organisms/ProfileSection';
-import { fetchLoggedInUser } from '../actions/auth';
 
-const initialState = {
-    name: '',
-    surname: '',
-    position: '',
-}
 
-const UserPage = (props) => {
-
-    const [userData, setUserData] = useState(initialState);
-    const userAuth = useSelector(state => state.auth);
-    useEffect(() => {
-        setUserData(userAuth)
-    }, [])
-
+const UserPage = () => {
 
     return(
         <>
