6e72c4b4858ec0af6516401c10da9a7d568c9227
Bartlomiej95 <panfilbartlomiej@gmail.com>
2021-03-10 20:54:59 +0100

    Add redux-persist to project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import reducers from './reducers/index';
 import App from './App';
+
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+    
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem('state')
+    if(serializedState === null) return undefined
+    return JSON.parse(serializedState);
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
+}
 
 const composeEnhancers =
   typeof window === 'object' &&
@@ -17,9 +40,16 @@ const enhancer = composeEnhancers(
   applyMiddleware(thunk),
   // other store enhancers if any
 );
-const store = createStore(reducers, enhancer);
+
+const persistedState = loadFromLocalStorage();
+
+const store = createStore(reducers, persistedState, enhancer);
 
 // const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
+
+
 
 ReactDOM.render(
 
@@ -29,3 +59,4 @@ ReactDOM.render(
 ,
   document.getElementById('root')
 );
+
